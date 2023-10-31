6302e3a835e9e1f0753558db7e6cfbff8d2a9720
Elaidzha1940 <eli080293@gmail.com>
2023-10-31 18:18:19 +0300

    Add overlay

--- MaskModifier/ContentView.swift
+++ MaskModifier/ContentView.swift
@@ -15,15 +15,33 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            HStack {
-                ForEach(1..<6) { index in
-                    Image(systemName: "staroflife.fill")
-                        .font(.system(size: 35))
-                        .foregroundColor(rating >= index ? Color.orange : Color.gray)
-                        .onTapGesture {
-                            rating = index
-                        }
+            starsView
+                .overlay {
+                    overlayView
                 }
+        }
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+                
+            }
+        }
+    }
+    
+    private var starsView: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: "staroflife.fill")
+                    .font(.system(size: 35))
+                    .foregroundColor(rating >= index ? Color.orange : Color.gray)
+                    .onTapGesture {
+                        rating = index
+                    }
             }
         }
     }
