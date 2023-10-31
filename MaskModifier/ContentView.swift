70370b556d200e5e6b01940e04708d634302404c
Elaidzha1940 <eli080293@gmail.com>
2023-10-31 04:24:31 +0300

    MaskModifier

---
+++ MaskModifier/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: MaskModifier
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 31.10.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+       
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MaskModifier/MaskModifierApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: MaskModifier
+//  File: MaskModifierApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 31.10.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct MaskModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
